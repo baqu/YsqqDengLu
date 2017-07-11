431f87f2f64e04e7182c589959afb50755764eb1
baqu <321188323@qq.com>
2017-07-11 20:32:31 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/macbook/ysqqdenglu/Baqu.java
@@ -0,0 +1,21 @@
+package com.bawei.macbook.ysqqdenglu;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * 类的用途 ：
+ * zhangjiale
+ * {DATE}
+ */
+
+public class Baqu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.baqu);
+    }
+}
